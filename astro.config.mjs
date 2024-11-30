71e43f9bc26757d84bea3a75c235e1c0292ab5f1
Bruno Garcia <bgarcia@hoodriverconsulting.com>
2024-11-30 17:03:20 -0500

    inital dependencies installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,15 @@ import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify';
 import tailwind from '@astrojs/tailwind';
 
+import react from '@astrojs/react';
+
+import db from '@astrojs/db';
+
 // https://astro.build/config
 export default defineConfig({
-    integrations: [
-        tailwind({
-            applyBaseStyles: false
-        })
-    ],
+    integrations: [tailwind({
+        applyBaseStyles: false
+    }), react(), db()],
     output: 'hybrid',
     adapter: netlify()
 });
